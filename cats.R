437a8b30df32fe0b0cfb6173c1764eadd392dae0
Lucy Lee <lucylee210@gmail.com>
2018-07-10 09:39:00 -0400

    Add 2 dummy scripts

---
+++ cats.R
@@ -0,0 +1,12 @@
+
+# Idea for this function modified from Hilary
+# Parker's R Package tutorial: https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+
+
+cats <- function(love = TRUE){
+  if (love == TRUE){
+    print("I love cats!")
+  } else {
+    print("I am a soulless individual.")
+  }
+}
---
+++ mycalc.R
@@ -0,0 +1,36 @@
+#' My Calc
+#'
+#' This function performs basic arithmetic operations including
+#'    addition, subtration, division, and multiplication. The
+#'    user specifies two numeric arguments and one operand argument.
+#' @param a,b numeric vector or matrix
+#' @param oper string describing the operand to use. Valid values
+#'    for this parameter include "sum", "subtract", "divide", and "multiply".
+#' @export
+#' @examples
+#' mycalc(1:5, c(5, 2, 3, 10, 2), "divide")
+#' mycalc(matrix(1:9, nrow = 3, ncol = 3), 10, "multiply")
+#'
+#' \dontrun{
+#' mycalc(1:5, c(5, 2, 3, 10, 2), "subtrat")
+#' }
+
+
+mycalc <- function(a, b, oper) {
+  if (typeof(a) == "character" || typeof(b) == "character") {
+    stop("Objects of type 'character' are not supported in this function.", call. = FALSE)
+  } else {
+    if (oper == "sum") {
+      result <- a + b
+    } else if (oper == "subtract") {
+      result <- a - b
+    } else if (oper == "divide") {
+      result <- a / b
+    } else if (oper == "multiply") {
+      result <- a * b
+    } else {
+      stop("Your operator is not recognized. Please check your spelling.", call. = FALSE)
+    }
+    return(result)
+  }
+}
